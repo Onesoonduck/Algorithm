6a115754d27387a0bc09493d82ce6dd8858e21f3
Donggeun <soonduck3729@gmail.com>
2024-09-02 14:35:53 +0900

    [level 0] Title: 아이스 아메리카노, Time: 0.02 ms, Memory: 75.9 MB -BaekjoonHub

--- 프로그래머스/0/120819. 아이스 아메리카노/아이스 아메리카노.java
+++ 프로그래머스/0/120819. 아이스 아메리카노/아이스 아메리카노.java
@@ -2,7 +2,7 @@ class Solution {
     public int[] solution(int money) {
         int num1 = money / 5500;
         int num2 = money % 5500;
-        int[] answer = new int[] { num1, num2 };
+        int[] answer = { num1, num2 };
 
         return answer;
     }
