3ba2654ae7732dd8f9c6e146220790d870ea9b50
Donggeun, Kim <soonduck3729@gmail.com>
2024-09-09 15:54:27 +0900

    [Bronze V] Title: A+B - 7, Time: 128 ms, Memory: 16068 KB -BaekjoonHub

---
+++ 백준/Bronze/11021. A＋B － 7/A＋B － 7.java
@@ -0,0 +1,24 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int A = Integer.parseInt(br.readLine());
+
+        StringTokenizer st;
+
+        for (int i = 0; i < A; i++) {
+            st = new StringTokenizer(br.readLine(), " ");
+            bw.write("Case #" + (i + 1) + ": " + (Integer.parseInt(st.nextToken()) + Integer.parseInt(st.nextToken()))
+                    + "\n");
+        }
+        br.close();
+
+        bw.flush();
+        bw.close();
+
+    }
+}
