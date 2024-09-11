4513163f6a4b70b500140cb57b5f70abee65a40e
Donggeun, Kim <soonduck3729@gmail.com>
2024-09-11 15:04:37 +0900

    [Bronze V] Title: 문자와 문자열, Time: 100 ms, Memory: 14080 KB -BaekjoonHub

---
+++ 백준/Bronze/27866. 문자와 문자열/문자와 문자열.java
@@ -0,0 +1,15 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String args[]) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String S = br.readLine();
+        int i = Integer.parseInt(br.readLine());
+        Character str = S.charAt(i - 1);
+
+        System.out.println(str.toString());
+    }
+}
