fd00b237f73d8c20eb737237a46d772f6f0f33cc
Donggeun, Kim <soonduck3729@gmail.com>
2024-09-12 14:05:56 +0900

    [Bronze II] Title: 단어의 개수, Time: 308 ms, Memory: 39468 KB -BaekjoonHub

---
+++ 백준/Bronze/1152. 단어의 개수/단어의 개수.java
@@ -0,0 +1,21 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String args[]) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        String str = "";
+        int count = 0;
+
+        while (st.hasMoreTokens()) {
+            str = st.nextToken();
+            count++;
+        }
+
+        System.out.println(count);
+
+    }
+}
